70e3ab0ecffce3b4b7939ee95c94d9bd658f41e6
Vlad-dualv <147710428+Vlad-dualv@users.noreply.github.com>
2024-06-28 18:10:31 +0100

    Update

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 let currentMoleTile;
-let currentPlantTile;
+let currentPlantTile1;
+let currentPlantTile2;
+let currentPlantTile3;
+let previousMoleTile;
+let previousPlantTile1;
+let previousPlantTile2;
+let previousPlantTile3;
 let score = 0;
-let level = 1;
 let gameOver = false;
 
 let restartButton = document.getElementById("restart");
@@ -20,48 +25,81 @@ function setGame() {
     }
 
     setInterval(setMole, 1000);
-    setInterval(setPlant, 2000);
+    setInterval(setPlant1, 2000);
+    setInterval(setPlant2, 2000);
+    setInterval(setPlant3, 2000);
 }
 
-function getRandomTile() {
-    let num = Math.floor(Math.random() * 9);
-    return num.toString();
+function getRandomTile(excludeTiles = []) {
+    let num;
+    do {
+        num = Math.floor(Math.random() * 9).toString();
+    } while (excludeTiles.includes(num));
+    return num;
 }
 
 function setMole() {
     if (gameOver) {
         return;
     }
-    if(currentMoleTile) {
-    currentMoleTile.innerHTML = "";
+    if (currentMoleTile) {
+        currentMoleTile.innerHTML = "";
     }
 
     let mole = document.createElement("img");
     mole.src = "./src/monty-mole.png";
-    let num = getRandomTile();
-    if(currentPlantTile && currentPlantTile.id === num) {
-        return;
-    }
+    let num = getRandomTile([currentPlantTile1?.id, currentPlantTile2?.id, currentPlantTile3?.id, previousMoleTile]);
+    previousMoleTile = num;
     currentMoleTile = document.getElementById(num);
     currentMoleTile.appendChild(mole);
 }
 
-function setPlant() {
+function setPlant1() {
     if (gameOver) {
         return;
     }
-    if(currentPlantTile) {
-        currentPlantTile.innerHTML = "";
+    if (currentPlantTile1) {
+        currentPlantTile1.innerHTML = "";
     }
 
-    let plant = document.createElement("img");
-    plant.src = "./src/piranha-plant.png";
-    let num = getRandomTile();
-    if (currentMoleTile && currentMoleTile.id === num) {
+    let plant1 = document.createElement("img");
+    plant1.src = "./src/piranha-plant.png";
+    let num = getRandomTile([currentMoleTile?.id, currentPlantTile2?.id, currentPlantTile3?.id, previousPlantTile1]);
+    previousPlantTile1 = num;
+    currentPlantTile1 = document.getElementById(num);
+    currentPlantTile1.appendChild(plant1);
+}
+
+function setPlant2() {
+    if (gameOver) {
         return;
     }
-    currentPlantTile = document.getElementById(num);
-    currentPlantTile.appendChild(plant);
+    if (currentPlantTile2) {
+        currentPlantTile2.innerHTML = "";
+    }
+
+    let plant2 = document.createElement("img");
+    plant2.src = "./src/piranha-plant.png";
+    let num = getRandomTile([currentMoleTile?.id, currentPlantTile1?.id, currentPlantTile3?.id, previousPlantTile2]);
+    previousPlantTile2 = num;
+    currentPlantTile2 = document.getElementById(num);
+    currentPlantTile2.appendChild(plant2);
+}
+
+function setPlant3() {
+    if (gameOver) {
+        return;
+    }
+    if (currentPlantTile3) {
+        currentPlantTile3.innerHTML = "";
+    }
+
+    let plant3 = document.createElement("img");
+    plant3.src = "./src/piranha-plant.png";
+    let num = getRandomTile([currentMoleTile?.id, currentPlantTile1?.id, currentPlantTile2?.id, previousPlantTile3]);
+    previousPlantTile3 = num;
+    currentPlantTile3 = document.getElementById(num);
+    currentPlantTile3.appendChild(plant3);
 }
 
 function selectTile() {
@@ -74,25 +112,15 @@ function selectTile() {
         currentMoleTile.firstChild.style.border = "1px solid black";
         score += 10;
         document.getElementById("score").innerText = `Score: ${score.toString()}`;
-        if (score >= 30) {
-            gameOver = true;
-            document.getElementById("game-board").classList.add("level-two");
-            document.getElementById("game-board").innerHTML = "<h2>LEVEL 2</h2>";
-            setTimeout(nextLevel, 3000);
-        }
-    } else if (this === currentPlantTile) {
-        currentPlantTile.firstChild.style.backgroundColor = "red";
-        currentPlantTile.firstChild.style.borderRadius = "50%";
-        currentPlantTile.firstChild.style.border = "1px solid black";
+    } else if (this === currentPlantTile1 || this === currentPlantTile2 || this === currentPlantTile3) {
+        this.firstChild.style.backgroundColor = "red";
+        this.firstChild.style.borderRadius = "50%";
+        this.firstChild.style.border = "1px solid black";
         document.getElementById("score").innerText = "GAME OVER " + "\n" + `Your Score: ${score.toString()}`;
         document.getElementById("score").style.color = "red";
         gameOver = true;
         restartButton.style.display = "block";
     }
-}
-
-function nextLevel () {
-    location.reload();
 }
 
 restartButton.addEventListener("click", function() {
